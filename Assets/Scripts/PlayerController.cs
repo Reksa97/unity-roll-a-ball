bd59bc096fd7e9ec56b98e4210940849786f5166
Reksa97 <xrexrexr@sf-2.cs.helsinki.fi>
2019-09-09 12:59:36 +0300

    tutorial completed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     // Update is called once per frame
@@ -34,6 +44,16 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 15) {
+            winText.text = "You win!!!";
         }
     }
 
